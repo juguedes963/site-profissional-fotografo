cee55d76148c419f09b27a62f230b801475598f4
Julio Cesar Oliveira Guedes L Silva <11546127917@itajai.local>
2024-04-12 17:03:48 -0300

    ajustes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,96 +7,9 @@ import Line from '../../components/Line';
 import { IoLogoWhatsapp } from "react-icons/io";
 import { PiInstagramLogoFill } from "react-icons/pi";
 import { MdEmail } from "react-icons/md";
+import { photos, photosMatch, photosNew } from '../../constants';
 
-const photos = [
-    {
-        photos: require('../../assets/casuais/c1.jpeg')
-    },
-    {
 
-        photos: require('../../assets/casuais/c2.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casuais/c3.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casuais/c4.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casuais/c5.jpeg')
-    },
-]
-const photosMatch = [
-    {
-        photos: require('../../assets/casamento/casa1.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casamento/casa2.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casamento/casa3.jpeg')
-    },
-    {
-        photos: require('../../assets/casamento/make1.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casamento/make2.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casamento/make3.jpeg')
-    },
-    {
-        photos: require('../../assets/casamento/make4.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casamento/make5.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casamento/make6.jpeg')
-    },
-    {
-
-        photos: require('../../assets/casamento/make7.jpeg')
-    },
-    {
-
-        photos: require('../../assets/pre/pre1.jpeg')
-    },
-    {
-
-        photos: require('../../assets/pre/pre2.jpeg')
-    },
-]
-const photosNew = [
-    {
-        photos: require('../../assets/new/new1.jpeg')
-    },
-    {
-
-        photos: require('../../assets/new/new2.jpeg')
-    },
-    {
-
-        photos: require('../../assets/new/new3.jpeg')
-    },
-    {
-
-        photos: require('../../assets/new/new4.jpeg')
-    },
-    {
-
-        photos: require('../../assets/new/new5.jpeg')
-    },
-]
 export default function Home() {
     const breakpointColumnsObj = {
         default: 4,
